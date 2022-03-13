a62ffbaa50f3d404ffbf27d49952a56af7e1f3a6
Spenser D <1337spenser@gmail.com>
2022-03-13 14:17:16 -0400

    Chapter 1, exercises 01 through 03

--- Chapter_1/01-03/main.c
+++ Chapter_1/01-03/main.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 
 int main() {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;
@@ -16,9 +16,11 @@ int main() {
     step = 20;
 
     fahr = lower;
+    printf("Fahrenheit\tCelsisus\n");
+    printf("************************\n");
     while(fahr <= upper) {
-        celsius =5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f \t\t %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 
